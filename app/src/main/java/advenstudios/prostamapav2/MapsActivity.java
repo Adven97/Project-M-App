d652e2094363b5e1360bf038bc3ed74661313219
Adam Tomczak <atomczak30@gmail.com>
2018-11-06 00:15:09 +0100

    dodana szufladka

--- app/src/main/java/advenstudios/prostamapav2/MapsActivity.java
+++ app/src/main/java/advenstudios/prostamapav2/MapsActivity.java
@@ -10,10 +10,16 @@ import android.os.Bundle;
 import android.provider.Settings;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
+import android.support.design.widget.NavigationView;
 import android.support.v4.app.ActivityCompat;
 import android.support.v4.content.ContextCompat;
+import android.support.v4.view.GravityCompat;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
 import android.util.Log;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.TextView;
 import android.widget.Toast;
@@ -86,8 +92,7 @@ public class MapsActivity extends AppCompatActivity implements OnMapReadyCallbac
 
     Marker marker;
 
-
-
+    private DrawerLayout mDrawerLayout;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -129,7 +134,71 @@ public class MapsActivity extends AppCompatActivity implements OnMapReadyCallbac
 
         mGoogleApiClient.connect();
 
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        ActionBar actionbar = getSupportActionBar();
+        actionbar.setDisplayHomeAsUpEnabled(true);
+        actionbar.setHomeAsUpIndicator(R.drawable.burger);
+
+
+        mDrawerLayout = findViewById(R.id.drawer_layout);
+        mDrawerLayout.addDrawerListener(
+                new DrawerLayout.DrawerListener() {
+                    @Override
+                    public void onDrawerSlide(View drawerView, float slideOffset) {
+                        // Respond when the drawer's position changes
+                    }
+
+                    @Override
+                    public void onDrawerOpened(View drawerView) {
+                        // Respond when the drawer is opened
+                    }
+
+                    @Override
+                    public void onDrawerClosed(View drawerView) {
+                        // Respond when the drawer is closed
+                    }
+
+                    @Override
+                    public void onDrawerStateChanged(int newState) {
+                        // Respond when the drawer motion state changes
+                    }
+                }
+        );
+
+
+        NavigationView navigationView = findViewById(R.id.nav_view);
+        navigationView.setNavigationItemSelectedListener(
+                new NavigationView.OnNavigationItemSelectedListener() {
+                    @Override
+                    public boolean onNavigationItemSelected(MenuItem menuItem) {
+                        // set item as selected to persist highlight
+                        menuItem.setChecked(true);
+                        // close drawer when item is tapped
+                        mDrawerLayout.closeDrawers();
+
+                        // Add code here to update the UI based on the item selected
+                        // For example, swap UI fragments here
+
+                        return true;
+                    }
+                });
     }
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case android.R.id.home:
+                mDrawerLayout.openDrawer(GravityCompat.START);
+                return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+
+
+
+
+
 
     /**
      * Saves the state of the map when the activity is paused.
@@ -369,7 +438,6 @@ public class MapsActivity extends AppCompatActivity implements OnMapReadyCallbac
 
     private void dodajNowyMarker( double geoLat, double geoLong, BitmapDescriptor myIcon){
 
-
         if(marker != null && markerNumber > 0){
             marker.remove();}
 
@@ -379,7 +447,6 @@ public class MapsActivity extends AppCompatActivity implements OnMapReadyCallbac
                     .title("You're here")
                     .snippet("")
                     .icon(icon));
-
         }
 
 
